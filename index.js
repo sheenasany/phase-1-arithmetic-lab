e950bb9d79204b4de11ac6dc2a4b7613c8d9b6dd
Sheena Sang <sheenasany@gmail.com>
2022-07-18 13:53:46 -0400

    commit tree 26a6d691c6b719fe8c4688afad28b8a3b56ad6fb

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+const num1 = 31 
+const num2 = 2
+const multiply = num1 * num2
 
-// Write your code here
+//first create a variable with the name of random and include 
+// additional methods for arthimetic for the variable
+// first would be Math.round() because we want to make sure to round the number to a whole integer
+//rather than a decimal number.
+// within that () of Math.round, include Math.random to randomize whatever number, and multiply it by 10
+// so that it pushes it to the 10th power instead of having 0.1234 = 0 
+// then add a 1 to the end so that whatever random number appears which is a random number between 0 and 1 
+// will be pushed to the 10th power, rounded to the nearest integer and add 1 to make sure it's greater than 1 
+const random = Math.round(Math.random() * 10) + 1
+
+
+const num3 = 16
+const num4 = 6
+const mod = num3 % num4
+
+const max = Math.max(1, 5, 7, 15, 20)
