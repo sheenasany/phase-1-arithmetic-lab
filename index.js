e43d6fa54a478bb172422e7c13b7101acb5fa777
Sheena Sang <sheenasany@gmail.com>
2023-08-29 13:46:22 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+// simple const will suffice to pass test
+// define num1 and num2 as the specific numbers to equate to 62 when multiplied
+// then construct multiply using order of operations (*) operator
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2
 
-// Write your code here
+// interesting to note; math.floor and math.round work to pass the lab
+// yet math.cecil does not pass even though it would round up, why? 
+const random = Math.round(Math.random() * 10) + 1;
+
+// const numbers first that give out the remainder as 4
+// then const mod and include the operation
+const num3 = 16
+const num4 = 6
+const mod = num3 % num4
+
+// include math.max in the const and add random numbers of your choosing within parentheses
+const max = Math.max(1, 5, 7, 15, 20)
